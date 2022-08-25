26ab2433ac1239cf589239b45d5cc4131501a441
unknown <yurijlitviniii@gmail.com>
2022-08-25 21:48:19 +0300

    CreateArea.jsx: Create react component; App.jsx: Add CreateArea to render; Note.jsx: Add 'DELETE' button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,18 @@ import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map((note) => (
+            <CreateArea />
+            {notes.map((noteItem) => (
                 <Note 
-                    key = {note.id}
-                    title = {note.title}
-                    content = {note.content}
+                    key = {noteItem.key}
+                    title = {noteItem.title}
+                    content = {noteItem.content}
                 />
             ))}
             <Footer />
