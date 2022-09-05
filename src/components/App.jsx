adcea87dfda9d4b7ca6ce757f9c4e478cde783fa
unknown <yurijlitviniii@gmail.com>
2022-09-05 21:42:08 +0300

    App.jsx: Update 'note' import; CreateArea.jsx: Update 'note' handle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import defaultNotes from "../notes";
 import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+    
+    function addItem(note) {
+        setNotes(prevItems => {
+            return [
+                ...prevItems,
+                note
+            ]
+        });
+    }
+
     return (
         <div>
             <Header />
-            <CreateArea />
-            {notes.map((noteItem) => (
+            <CreateArea onAdd = {addItem}/>
+            {notes.map((note, index) => (
                 <Note 
-                    key = {noteItem.key}
-                    title = {noteItem.title}
-                    content = {noteItem.content}
+                    key = {index}
+                    title = {note.title}
+                    content = {note.content}
                 />
             ))}
             <Footer />
