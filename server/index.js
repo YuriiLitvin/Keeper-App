8a6aced6a4b676675a81da62fb8f2aa3f2723d44
unknown <yurijlitviniii@gmail.com>
2022-09-16 19:26:41 +0300

    Relocate client side of the the app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./db");
 
+
 const app = express();
+
+app.use(express.static(path.resolve(_dirname, "../client/build")));
 app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
